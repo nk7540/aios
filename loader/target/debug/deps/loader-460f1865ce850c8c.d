48874d8ec838181bbbac05b5f8fbbf42d4d0533a
ryo-naka <ryo.prfm27@gmail.com>
2022-09-29 20:28:56 +0900

    first commit

---
+++ loader/target/debug/deps/loader-460f1865ce850c8c.d
@@ -0,0 +1,5 @@
+/home/cs_gakusei/work/aios/loader/target/debug/deps/loader-460f1865ce850c8c: src/main.rs
+
+/home/cs_gakusei/work/aios/loader/target/debug/deps/loader-460f1865ce850c8c.d: src/main.rs
+
+src/main.rs:
---
+++ loader/target/debug/loader.d
@@ -0,0 +1,1 @@
+/home/cs_gakusei/work/aios/loader/target/debug/loader: /home/cs_gakusei/work/aios/loader/src/main.rs
