4ed181bda3742d8ee7ecab614eb0810312cbf132
ryo-naka <ryo.prfm27@gmail.com>
2022-09-30 13:18:44 +0900

    feat: get memory map

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -4,13 +4,37 @@
 use core::panic::PanicInfo;
 use core::fmt::Write;
 
-use uefi::data_types::Handle;
-use uefi::table::{Boot, SystemTable};
-use uefi::Status;
+use uefi::prelude::*;
+use uefi::table::boot::MemoryType;
+
+struct MemoryMap {
+    map_size: u64,
+    map_buffer: *mut u8,
+    map_key: u64,
+    descriptor_size: u64,
+    descriptor_version: u32,
+}
+
+const EFI_PAGE_SIZE: u64 = 0x1000;
 
 #[no_mangle]
-pub fn efi_main(image_handle: uefi::Handle, mut system_table: SystemTable<Boot>) -> Status {
-    system_table.stdout().write_str("Hello, World!");
+pub fn efi_main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    writeln!(system_table.stdout(), "Hello, World!").unwrap();
+
+    let status: Status;
+    let bs = system_table.boot_services();
+
+    let mut buf = [0_u8; 4096 * 4];
+    let (_map_key, desc_itr) = bs
+        .memory_map(&mut buf)
+        .expect("Failed to get memory map");
+    desc_itr.for_each(|desc| {
+        if desc.ty == MemoryType::CONVENTIONAL {
+            writeln!(system_table.stdout(), "{:#x}: {} pages",
+                desc.phys_start, desc.page_count).unwrap();
+        }
+    });
+
 
     loop {}
 }
