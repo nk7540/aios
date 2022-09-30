e68ec2b4b6a95cdab1da17f1bfe7e2f650271232
ryo-naka <ryo.prfm27@gmail.com>
2022-09-30 19:24:45 +0900

    feat: load elf header

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -6,8 +6,12 @@ use core::ops::{Deref,DerefMut};
 use core::fmt::Write;
 
 use uefi::prelude::*;
-use uefi::proto::media::file::{Directory, File, FileMode, FileAttribute};
+use uefi::proto::console::text;
+use uefi::proto::media::file::{Directory, File, FileMode, FileAttribute, FileInfo};
 use uefi::table::boot::MemoryType;
+
+#[macro_use]
+extern crate alloc;
 
 struct MemoryMap {
     map_size: u64,
@@ -30,9 +34,43 @@ fn open_root_dir(bs: &BootServices, image_handle: Handle) -> Directory {
     file_system.deref_mut().open_volume().unwrap()
 }
 
+type Elf64_Addr = usize;
+type Elf64_Off = u64;
+type Elf64_XWord = u64;
+type Elf64_Word = u32;
+type Elf64_Half = u16;
+
+#[repr(C)]
+struct Elf64_Ehdr {
+    a: [u8; 24],
+    e_entry: Elf64_Addr,
+    e_phoff: Elf64_Off,
+    b: [u8; 14],
+    e_phentsize: Elf64_Half,
+    e_phnum: Elf64_Half,
+}
+
+struct Elf64_Phdr {
+    p_type: Elf64_Word,
+    p_flags: Elf64_Word,
+    p_offset: Elf64_Off,
+    p_vaddr: Elf64_Addr,
+    p_paddr: Elf64_Addr,
+    p_filesz: Elf64_XWord,
+    p_memsz: Elf64_XWord,
+    p_align: Elf64_XWord,
+}
+
+fn load_elf(buf: &mut [u8], stdout: &mut text::Output) {
+    let ehdr_ptr = buf.as_ptr() as *const Elf64_Ehdr;
+    let ehdr = unsafe { &*ehdr_ptr };
+    writeln!(stdout, "e_entry: {:#x}", ehdr.e_entry);
+}
+
 #[entry]
 pub fn efi_main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
+    system_table.stdout().reset(false).unwrap();
     writeln!(system_table.stdout(), "Hello, World!").unwrap();
 
     // let bs = system_table.boot_services().clone();
@@ -49,14 +87,19 @@ pub fn efi_main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> St
     });
 
     let mut root_dir = open_root_dir(system_table.boot_services(), image_handle);
-    let file = root_dir.open(
+    let mut kernel_file = root_dir.open(
         cstr16!("kernel.elf"),
         FileMode::Read,
         FileAttribute::READ_ONLY
     ).expect("Failed to open file");
     writeln!(system_table.stdout(), "File opened.").unwrap();
-    file.close();
-    writeln!(system_table.stdout(), "File closed.").unwrap();
+
+    let kernel_file_info = kernel_file.get_boxed_info::<FileInfo>().unwrap();
+    let mut buf = vec![0; kernel_file_info.as_ref().file_size() as usize];
+    kernel_file.into_regular_file().unwrap().read(&mut buf);
+    load_elf(&mut buf, system_table.stdout());
+
+    writeln!(system_table.stdout(), "File read.").unwrap();
 
     loop {}
 }
