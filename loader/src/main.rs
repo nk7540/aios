c76aa0cede5e30efb568ecba605a65eedbd12dab
ryo-naka <ryo.prfm27@gmail.com>
2022-09-30 18:01:15 +0900

    feat: load kernel

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
+#![feature(abi_efiapi)]
 
-use core::panic::PanicInfo;
+use core::ops::{Deref,DerefMut};
 use core::fmt::Write;
 
 use uefi::prelude::*;
+use uefi::proto::media::file::{Directory, File, FileMode, FileAttribute};
 use uefi::table::boot::MemoryType;
 
 struct MemoryMap {
@@ -17,15 +19,26 @@ struct MemoryMap {
 
 const EFI_PAGE_SIZE: u64 = 0x1000;
 
-#[no_mangle]
+fn open_root_dir(bs: &BootServices, image_handle: Handle) -> Directory {
+    use uefi::proto::loaded_image::LoadedImage;
+    use uefi::proto::media::fs::SimpleFileSystem;
+
+    let loaded_image = bs
+        .open_protocol_exclusive::<LoadedImage>(image_handle).unwrap();
+    let mut file_system = bs
+        .open_protocol_exclusive::<SimpleFileSystem>(loaded_image.deref().device()).unwrap();
+    file_system.deref_mut().open_volume().unwrap()
+}
+
+#[entry]
 pub fn efi_main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap();
     writeln!(system_table.stdout(), "Hello, World!").unwrap();
 
-    let status: Status;
-    let bs = system_table.boot_services();
+    // let bs = system_table.boot_services().clone();
 
     let mut buf = [0_u8; 4096 * 4];
-    let (_map_key, desc_itr) = bs
+    let (_map_key, desc_itr) = system_table.boot_services()
         .memory_map(&mut buf)
         .expect("Failed to get memory map");
     desc_itr.for_each(|desc| {
@@ -35,11 +48,15 @@ pub fn efi_main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> St
         }
     });
 
+    let mut root_dir = open_root_dir(system_table.boot_services(), image_handle);
+    let file = root_dir.open(
+        cstr16!("kernel.elf"),
+        FileMode::Read,
+        FileAttribute::READ_ONLY
+    ).expect("Failed to open file");
+    writeln!(system_table.stdout(), "File opened.").unwrap();
+    file.close();
+    writeln!(system_table.stdout(), "File closed.").unwrap();
 
-    loop {}
-}
-
-#[panic_handler]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
     loop {}
 }
